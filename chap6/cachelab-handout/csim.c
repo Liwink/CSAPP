c0cbe9b89b23cff32a42c2baad9c042de2b907a4
Liwink <lyh.next@gmail.com>
2018-03-26 23:59:15 +0800

    cachelab: part1 done?

--- chap6/cachelab-handout/csim.c
+++ chap6/cachelab-handout/csim.c
@@ -1,21 +1,22 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 #include "math.h"
 #include "unistd.h"
 #include "cachelab.h"
 
-void getoptions(int argc, char *argv[], int *s, int *E, int *b, char **t) {
+void getoptions(int argc, char *argv[], int *S, int *E, int *B, char **t) {
     int opt;
     while ((opt = getopt(argc, argv, "s:E:b:t:")) != -1) {
         switch (opt) {
             case 's':
-                *s = *optarg - 48;
+                *S = pow(2, *optarg - 48);
                 break;
             case 'E':
                 *E = *optarg - 48;
                 break;
             case 'b':
-                *b = *optarg - 48;
+                *B = pow(2, *optarg - 48);
                 break;
             case 't':
                 *t = optarg;
@@ -30,19 +31,18 @@ typedef struct POSITION {
     int addr;
 } Position;
 
-void readLine(Position *pos, char *line, int bVal, int sVal) {
+void readLine(Position *pos, char *line, int BVal, int SVal) {
     char addr[10];
     sscanf(line, "%s %[^,]", &pos->type, addr);
-    printf("type: %s; addr: %s\n", &pos->type, addr);
     pos->addr = (int) strtol(addr, NULL, 16);
-    int tmp = pos->addr / pow(2, bVal);
-    pos->set = tmp % (int) pow(2, sVal);
-    pos->tag = tmp / (int) pow(2, sVal);
+    int tmp = pos->addr / BVal;
+    pos->set = tmp % (int) SVal;
+    pos->tag = tmp / (int) SVal;
 }
 
 int main(int argc, char *argv[])
 {
-    int sVal, EVal, bVal;
+    int SVal, EVal, BVal;
     char *tVal;
 
     FILE *fp;
@@ -51,42 +51,70 @@ int main(int argc, char *argv[])
 
     size_t cache_size;
 
-    getoptions(argc, argv, &sVal, &EVal, &bVal, &tVal);
+    getoptions(argc, argv, &SVal, &EVal, &BVal, &tVal);
 
     // printf("s: %d\n", sVal);
     // printf("E: %d\n", EVal);
     // printf("b: %d\n", bVal);
     // printf("t: %s\n", tVal);
 
-    cache_size = EVal * pow(2, sVal);
+    // [set0_e0_tag, set0_e0_count, set0_e1_tag, set0_e1_count]
+    cache_size = EVal * SVal * 2;
     printf("cache size: %zu\n", cache_size);
     int *cache = (int *) malloc(cache_size);
-    cache[0] = 1;
+    memset(cache, 0, sizeof (int) * cache_size);
+    int j;
+    for (j = 0; j < cache_size; j++) {
+        cache[j] = -1;
+        printf("%d,", cache[j]);
+    }
 
-    char *s_line = "L 4001ac,1";
     Position pos;
-    readLine(&pos, s_line, bVal, sVal);
-    printf("addr: %x\n", pos.addr);
-    printf("set : %x\n", pos.set);
-    printf("tag : %x\n", pos.tag);
-    printf("type: %c\n", pos.type);
-
-    // char type[10], addr[10];
-    // char *s_line = "L 4001ac,1";
-    // sscanf(s_line, "%s %[^,]", type, addr);
-    // printf("type: %s; addr: %s\n", type, addr);
-    // int num = (int) strtol(addr, NULL, 16);
-    // int num1 = num / pow(2, bVal);
-    // int num2 = num1 % (int) pow(2, sVal);
-    // printf("num: %d; %x\n", num, num);
-    // printf("   : %d; %x\n", num1, num1);
-    // printf("   : %d; %x\n", num2, num2);
-    // printf("   : %x\n", num % pow(2, sVal));
-
+    int hit = 0, miss = 0, evict = 0;
+    int start, i;
+    int is_hit, is_evict;
+    int small_count, small_index;
+    int count = 0;
     fp = fopen(tVal, "r");
-    while (getline(&line, &len, fp) != -1)
-        printf("%s", line);
+    while (getline(&line, &len, fp) != -1) {
+        readLine(&pos, line, BVal, SVal);
+        is_hit = 0;
+        is_evict = 1;
+        small_count = count;
+        printf("addr: %x; set: %x; tag: %x; type: %c\n", pos.addr, pos.set, pos.tag, pos.type);
+        start = pos.set * EVal * 2;
+        for (i = 0; i < EVal; i++) {
+            if (cache[start + i * 2] == pos.tag) {
+                is_hit = 1;
+                break;
+            }
+            else if (cache[start + i * 2 + 1] == -1) {
+                is_evict = 0;
+                small_index = i;
+                break;
+            }
+            else if (cache[start + i * 2 + 1] <= small_count) {
+                small_index = i;
+            }
+        }
 
-    printSummary(0, 0, 0);
+        if (is_hit)
+            hit++;
+        else {
+            miss++;
+            cache[start + small_index * 2] = pos.tag;
+            cache[start + small_index * 2 + 1] = small_count;
+            if (is_evict == 1)
+                evict++;
+        }
+
+        if (pos.type == 'M')
+            hit++;
+        printf("hit: %d; evict: %d\n", is_hit, is_evict);
+
+        count++;
+    }
+
+    printSummary(hit, miss, evict);
     return 0;
 }
