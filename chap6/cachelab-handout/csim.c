d3f22c8f5e94783935c2a68b7c49ac819825cdcc
Liwink <lyh.next@gmail.com>
2018-03-26 14:49:21 +0800

    cachelab: command line options & read file

--- chap6/cachelab-handout/csim.c
+++ chap6/cachelab-handout/csim.c
@@ -1,7 +1,41 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
 #include "cachelab.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+    int opt;
+    int sVal, EVal, bVal;
+    char *tVal;
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 100;
+
+    while ((opt = getopt(argc, argv, "s:E:b:t:")) != -1) {
+        switch (opt) {
+            case 's':
+                sVal = *optarg - 48;
+                break;
+            case 'E':
+                EVal = *optarg - 48;
+                break;
+            case 'b':
+                bVal = *optarg - 48;
+                break;
+            case 't':
+                tVal = optarg;
+        }
+    }
+    printf("s: %d\n", sVal);
+    printf("E: %d\n", EVal);
+    printf("b: %d\n", bVal);
+    printf("t: %s\n", tVal);
+
+    fp = fopen(tVal, "r");
+    while (getline(&line, &len, fp) != -1)
+        printf("%s", line);
+
     printSummary(0, 0, 0);
     return 0;
 }
